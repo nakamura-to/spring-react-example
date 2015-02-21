6b34ceaff961159485a3d4af1ade77383a900236
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2015-02-21 20:29:58 +0900

    Prevent XSS

--- src/main/java/com/winterbe/react/MainController.java
+++ src/main/java/com/winterbe/react/MainController.java
@@ -7,6 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.fasterxml.jackson.core.SerializableString;
+import com.fasterxml.jackson.core.io.CharacterEscapes;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 /**
  * @author Benjamin Winterberg
  */
@@ -25,9 +29,40 @@ public class MainController {
         List<Comment> comments = service.getComments();
         React react = new React();
         String commentBox = react.renderCommentBox(comments);
+        ObjectMapper mapper = createMapper();
+        String data = mapper.writeValueAsString(comments);
         model.put("content", commentBox);
-        model.put("data", comments);
+        model.put("data", data);
         return "index";
     }
 
+    private ObjectMapper createMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.getFactory().setCharacterEscapes(new CustomCharacterEscapes());
+        return mapper;
+    }
+
+    @SuppressWarnings("serial")
+    private static class CustomCharacterEscapes extends CharacterEscapes {
+
+        private final int[] _asciiEscapes;
+
+        public CustomCharacterEscapes() {
+            _asciiEscapes = standardAsciiEscapesForJSON();
+            _asciiEscapes['/'] = CharacterEscapes.ESCAPE_STANDARD;
+            _asciiEscapes['<'] = CharacterEscapes.ESCAPE_STANDARD;
+            _asciiEscapes['>'] = CharacterEscapes.ESCAPE_STANDARD;
+            _asciiEscapes['+'] = CharacterEscapes.ESCAPE_STANDARD;
+        }
+
+        @Override
+        public int[] getEscapeCodesForAscii() {
+            return _asciiEscapes;
+        }
+
+        @Override
+        public SerializableString getEscapeSequence(int i) {
+            return null;
+        }
+    }
 }
