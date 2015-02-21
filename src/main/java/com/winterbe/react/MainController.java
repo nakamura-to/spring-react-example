8031a854de1c3c54d3b3705a1a6ba0f6a00e7dc3
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2015-02-21 11:14:22 +0900

    Use thymeleaf instead of jsp

--- src/main/java/com/winterbe/react/MainController.java
+++ src/main/java/com/winterbe/react/MainController.java
@@ -1,12 +1,11 @@
 package com.winterbe.react;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
-import java.util.Map;
 
 /**
  * @author Benjamin Winterberg
@@ -18,22 +17,18 @@ public class MainController {
 
     private React react;
 
-    private ObjectMapper mapper;
-
     @Autowired
     public MainController(CommentService service) {
         this.service = service;
         this.react = new React();
-        this.mapper = new ObjectMapper();
     }
 
     @RequestMapping("/")
     public String index(Map<String, Object> model) throws Exception {
         List<Comment> comments = service.getComments();
         String commentBox = react.renderCommentBox(comments);
-        String data = mapper.writeValueAsString(comments);
         model.put("content", commentBox);
-        model.put("data", data);
+        model.put("data", comments);
         return "index";
     }
 
